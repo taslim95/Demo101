0ddbb4328c4a2d417d3dc3a30a311497688f3c23
Taslim tassu <taslim95>
2024-10-29 05:22:01 +0530

    first Commit

---
+++ DemoTest/src/Mypackage/FirstTestClass.java
@@ -0,0 +1,14 @@
+package Mypackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestClass{
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+	}
+}
